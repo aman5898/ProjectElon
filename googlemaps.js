55529b945b28aa41f54cf9a9ce6140ebb987fa61
Aman <amankrsingh.2012@gmail.com>
2018-10-07 12:35:07 +0530

    Route for each danger point added

--- googlemaps.js
+++ googlemaps.js
@@ -1,8 +1,11 @@
 var arr = require('./map-points.json')
+var $=require('jquery');
 
 var dangerlocations_arr = arr.dangerzones
 var safelocations_arr = arr.safezones
 var map;
+var directionsService;
+var directionsDisplay;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -37,22 +40,22 @@ function initMap() {
         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
     // Instantiate a directions service.
-    directionsService = new google.maps.DirectionsService,
-    directionsDisplay = new google.maps.DirectionsRenderer({
-        map: map
-    }),
-
-    dangerlocations_arr.forEach(dangerpoint => {
-        calculateAndDisplayRoute(directionsService, directionsDisplay, dangerpoint, safelocations_arr);        
-    });
+    directionsService = new google.maps.DirectionsService;
+        directionsDisplay = new google.maps.DirectionsRenderer({
+            map: map
+        });
+        // calculateAndDisplayRoute(directionsService, directionsDisplay, dangerlocations_arr[0], safelocations_arr);
+    // dangerlocations_arr.forEach(dangerpoint => {
+    //     calculateAndDisplayRoute(directionsService, directionsDisplay, dangerpoint, safelocations_arr);        
+    // });
 
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay, dangerpoint, safepoint_arr) {
-    var counter=0;
+    var counter = 0;
     pointA = dangerpoint;
-    var minDist=3214000;
-    var safestPoint=safepoint_arr[0];
+    var minDist = 3214000;
+    var safestPoint = safepoint_arr[0];
     safepoint_arr.forEach(pointB => {
         directionsService.route({
             origin: pointA,
@@ -62,23 +65,40 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay, dangerpo
             if (status == google.maps.DirectionsStatus.OK) {
                 // directionsDisplay.setDirections(response);
                 var dist = response.routes[0].legs[0].distance.value;
-                // console.log(response.routes[0].legs[0].distance.value);
-                
-                if(dist<minDist){
-                    minDist=dist;
-                    safestPoint=pointB;
+                // console.log(response.routes[0].legs[0].distance.value);                
+                if (dist < minDist) {
+                    minDist = dist;
+                    safestPoint = pointB;
                 }
             } else {
                 // window.alert('Directions request failed due to ' + status);
             }
-            if (counter == safepoint_arr.length-1) {
+            if (counter == safepoint_arr.length - 1) {
                 console.log(JSON.stringify(dangerpoint) + " " + JSON.stringify(safestPoint) + " " + minDist);
-
+                DisplayRoute(directionsService, directionsDisplay, pointA, safestPoint)   
+                console.log(minDist);
+                distKm=minDist/1000;
+                $(".console").empty();
+                $(".console").append(`Total Distance = ${distKm} Km`);
             }
             counter++;
         });
-        
-    }); 
+
+    });
+}
+
+function DisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
+    directionsService.route({
+        origin: pointA,
+        destination: pointB,
+        travelMode: google.maps.TravelMode.DRIVING
+    }, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            // window.alert('Directions request failed due to ' + status);
+        }
+    });
 }
 
 function setMap() {
@@ -92,16 +112,16 @@ initMap();
 // function nearestSafePoint(directionsService, directionsDisplay) {
 //     // console.log(dangerlocations_arr);
 //     // console.log(safelocations_arr);
-    
+
 //     dangerlocations_arr.forEach(dangerpoint => {
 //         var min;
 //         var safestPoint;
-        
+
 //         safelocations_arr.forEach(saferpoint => {
-            
+
 //             var dist = calculateAndDisplayRoute(directionsService, directionsDisplay, dangerpoint, saferpoint)
 //             console.log("aman= "+dist);
-            
+
 //             if(dist<min){
 //                 min=dist;
 //                 safestPoint=saferpoint;
